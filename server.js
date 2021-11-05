213e065116c013b5359776bc8953864bc721e13b
Izaak2 <izaak.git@gmail.com>
2021-11-05 13:07:19 +0100

    some updateds on server

--- server.js
+++ server.js
@@ -26,18 +26,66 @@ var ingredients = [
 ];
 
 
-app.get("/", function(request, response) { // "/" means base URL
+app.get("/ingredients", function(request, response) { // "/" means base URL
   response.send(ingredients); // send converts message to JSON
 });
 
 
-app.post("/", function(request, response){
+app.post("/ingredients", function(request, response){
   var ingredient = request.body;
   if(!ingredient || ingredient.text === ""){
     response.status(500).send({error: "Your ingredient must have text"});
   }else {
     ingredients.push(ingredient);
     response.status(200).send(ingredients);
+  }
+});
+
+app.put("/ingredients/:ingredientId", function(request, response) {
+
+  var newText = request.body.text;
+
+  if (!newText || newText === "") {
+      response.status(500).send({error:"You must provide ingredient text"})
+  } else {
+      var objectFound = false;
+      for (var x = 0; x < ingredients.length; x++) {
+          var ing = ingredients[x];
+
+          if (ing.id === request.params.ingredientId) {
+              ingredients[x].text = newText;
+              objectFound = true;
+              break;
+          }
+      }
+      if(!objectFound) {
+        response.status(500).send({error:"Ingredient not found"});
+      }
+      response.send(ingredients);
+    }
+});
+
+app.delete("/ingredients", function(request, response){
+  var id = request.body.id;
+  if(!id || id === ""){
+    response.status(500).send({error:"ingredient does not exists"});
+  }else {
+
+    var objectFound = false;
+
+    for(var x = 0; x < ingredients.length; x++) {
+      if(id === ingredients[x].id) {
+        ingredients.splice(x, 1);
+        objectFound = true;
+        break;
+      }
+    }
+
+    if(!objectFound){
+      response.status(500).send({error:"Ingredient not found"});
+    }
+
+    response.send(ingredients);
   }
 });
 
