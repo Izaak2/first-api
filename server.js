5c98d47c64fbcec94502d43b68353477dab8c876
Izaak2 <izaak.git@gmail.com>
2021-11-05 07:20:38 +0100

    some chagnes to the server

--- server.js
+++ server.js
@@ -2,8 +2,43 @@ var express = require("express");
 var app = express();
 //var app = require("express")(); //Same as two lines above
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+  {
+    "id":"1",
+    "text":"Eggs"
+  },
+  {
+    "id":"2",
+    "text":"Milk"
+  },
+  {
+    "id":"3",
+    "text":"Juice"
+  },
+  {
+    "id":"4",
+    "text":"Bacon"
+  }
+];
+
+
 app.get("/", function(request, response) { // "/" means base URL
-  response.send("My first API!"); // send converts message to JSON
+  response.send(ingredients); // send converts message to JSON
+});
+
+
+app.post("/", function(request, response){
+  var ingredient = request.body;
+  if(!ingredient || ingredient.text === ""){
+    response.status(500).send({error: "Your ingredient must have text"});
+  }else {
+    ingredients.push(ingredient);
+    response.status(200).send(ingredients);
+  }
 });
 
 app.get("/funions", function(request, response) {
